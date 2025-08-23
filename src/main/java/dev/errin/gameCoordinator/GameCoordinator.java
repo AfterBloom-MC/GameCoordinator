1ae2573dceb616e5eb70e84175fa398da442f31c
ErrinDev <frenchgoos3@gmail.com>
2025-08-23 22:11:25 +0100

    initial commit

---
+++ src/main/java/dev/errin/gameCoordinator/GameCoordinator.java
@@ -0,0 +1,17 @@
+package dev.errin.gameCoordinator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GameCoordinator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
